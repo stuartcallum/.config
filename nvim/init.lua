1010eed9a0f0e979921413835cc1b4e92cdedaae
callum.stuart <callum.stuart@bullhorn.com>
2024-03-13 17:02:53 +1100

    update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,26 @@
-require "core"
-
-local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
-
-if custom_init_path then
-  dofile(custom_init_path)
-end
-
-require("core.utils").load_mappings()
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
--- bootstrap lazy.nvim!
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
-dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
-require "plugins"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+
+
+require("lazy").setup({
+	{ "nvim-treesitter/nvim-treesitter", cmd = "TSUpdate" },
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true},
+	{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }},
+	{ "nvim-tree/nvim-web-devicons" }
+	})
+
+vim.cmd([[colorscheme gruvbox]])
