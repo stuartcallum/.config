8293f4a392c914fce4ad9107bd2784eac54de84e
callum.stuart <callum.stuart@bullhorn.com>
2024-05-12 20:33:47 +1000

    update vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,13 +14,17 @@ vim.opt.rtp:prepend(lazypath)
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
+vim.g.netrw_bufsettings = 'noma nomod nonu nobl nowrap ro'
 
 
 require("lazy").setup({
 	{ "nvim-treesitter/nvim-treesitter", cmd = "TSUpdate" },
 	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true},
 	{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }},
-	{ "nvim-tree/nvim-web-devicons" }
+	{ "nvim-tree/nvim-web-devicons" },
+	--	{ "romgrk/barbar.nvim", dependencies = {"lewis6991/gitsigns.nvim", "nvim-tree/nvim-web-devicons"}, init = function() vim.g.barbar_auto_setup = true end},
+	{ "williamboman/mason.nvim" },
+	{ "mfussenegger/nvim-jdtls" }
 	})
 
 vim.cmd([[colorscheme gruvbox]])
